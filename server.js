37c087051f616b1e71698f6d943497bb2c87331f
Sachin Jha <sachinjhagc@gmail.com>
2026-05-14 12:23:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/tunes", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
